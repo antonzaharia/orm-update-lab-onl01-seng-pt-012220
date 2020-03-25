68f296de80395306c00dcc7a8872109bde44cbad
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-03-25 16:34:51 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -60,5 +60,12 @@ class Student
   end 
   
   def self.find_by_name(name)
+    sql = <<-SQL
+    SELECT * FROM students
+    WHERE name = ?
+    LIMIT 1
+    SQL
+    
+    DB[:conn].execute(sql, name)
 
 end
