eacb163015d5883bd1dea22981af194b9f175ab9
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-03-25 16:26:40 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -53,7 +53,11 @@ class Student
   end
   
   def self.new_from_db(row)
-    Student.new( row[0], row[1], row[2])
+    new_student = self.new
+    new_student.id = row[0]
+    new_student.name = row[1]
+    new_student.grade = row[2]
+    new_student
   end
 
 end
